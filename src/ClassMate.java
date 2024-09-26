f7e83c3c64f77eb5329792f3a2a78191aeef1cfe
Martin Simunek <public@e-mis.cz>
2024-09-26 15:10:58 +0200

    feat: add classmate + test code
    test code is in main method.

---
+++ src/ClassMate.java
@@ -0,0 +1,27 @@
+import java.time.LocalDate;
+
+public class ClassMate {
+    private String name;
+    private LocalDate birthDate;
+
+    public ClassMate(String name, LocalDate birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        ClassMate karel = new ClassMate("Karel", LocalDate.of(2000, 1, 1));
+        System.out.println(karel.getName());
     }
 }
